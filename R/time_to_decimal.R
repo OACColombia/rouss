9da2071926582c27ede6be061addd8422ce2f3d1
Orlando Acevedo-Charry <oacevedocharry@ufl.edu>
2024-07-10 18:59:22 -0400

    Adjust Description, including the Imports for six packages required to run this package

--- R/time_to_decimal.R
+++ R/time_to_decimal.R
@@ -2,7 +2,9 @@ require(tidyverse)
 #' Convert time (Hour Minute Second) to decimal number.
 #'
 #' @description
-#' This function helps to convert time information (Hour Minute Second) to decimal number. It was extracted from "Best Practices for Using eBird Data" by Matthew Strimas-Mackey, Wesley M. Hochachka, Viviana Ruiz-Gutierrez, Orin J. Robinson, Eliot T. Miller, Tom Auer, Steve Kelling, Daniel Fink, & Alison Johnston
+#' This function helps to convert time information (Hour Minute Second) to decimal number.
+#' It was extracted from "Best Practices for Using eBird Data" by Matthew Strimas-Mackey, Wesley M. Hochachka, Viviana Ruiz-Gutierrez, Orin J. Robinson, Eliot T. Miller, Tom Auer, Steve Kelling, Daniel Fink, & Alison Johnston.
+#' This function requires the package `lubridate`, included in `tidyverse`.
 #'
 #' @param x A character vector of hour minute second
 #'
@@ -10,9 +12,9 @@ require(tidyverse)
 #' @export
 #'
 #' @examples
-#' x <- "7 6 5"
+#' x <- "5:30:00 AM"
 #' time_to_decimal(x)
 time_to_decimal <- function(x) {
-  x <- hms(x, quiet = TRUE)
-  hour(x) + minute(x) / 60 + second(x) / 3600
+  x <- lubridate::hms(x, quiet = TRUE)
+  lubridate::hour(x) + lubridate::minute(x) / 60 + lubridate::second(x) / 3600
 }
