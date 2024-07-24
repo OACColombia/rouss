961779702c5fe1f6b5e258608e3ce7d96ab2a104
OACColombia <acevedocharry@gmail.com>
2024-07-23 22:55:03 -0400

    egss_remle - adding two parameters
    - REML conduct a numeric maximization for sigmasqr and tausqr, however from A26 and A28, we can estimate the trend parameter (theta) and the initial population (x0). Now this function returns four parameters needed to conduct a simulation with egss_sim

--- R/egss_remle.R
+++ R/egss_remle.R
@@ -17,13 +17,13 @@
 
 egss_remle <- function(yt,tt,fguess_egss){
 
-  # Time-vector starting in 0.
+  #Temporal vectors
   t.i         <- tt-tt[1];
-  # Number of time-series transitions
   q           <- length(t.i)-1;
-  # length of time-series
   qp1         <- q+1;
-  # initial guesses (r as estimated, sigmasq and tausq at log scale (?), x0 as estimated)
+  t.s     <- t.i[2:qp1] - t.i[1:q];
+
+  # initial guesses (sigmasq and tausq at log scale)
   guess.optim <- c(log(fguess_egss[2:3]))
   # numerical optimization
   optim.out   <- optim(par=guess.optim,
@@ -31,8 +31,41 @@ egss_remle <- function(yt,tt,fguess_egss){
                        method="Nelder-Mead",
                        yt=yt,
                        tt=t.i)
-  #Extract MLEs and AIC
-  remles        <- c(exp(optim.out$par[1:2]))
+
+  #extract parameters estimated by REML
+  sigmasq <- exp(optim.out$par)[1]
+  tausq <- exp(optim.out$par)[1]
+
+  #to estimate trend parameter (theta) and initial population (x0)
+  vx      <- matrix(0,qp1,qp1);
+  for(i in 1:q){
+    vx[((i+1):qp1),((i+1):qp1)] <- matrix(1,(qp1-i),(qp1-i))*t.i[(i+1)];
+  }
+  Sigma.mat     <- sigmasq*vx;
+  Itausq        <- matrix(rep(0,(qp1*qp1)),
+                          nrow=qp1,
+                          ncol=qp1);
+  diag(Itausq)  <- rep(tausq,qp1);
+  V             <- Sigma.mat + Itausq;
+  D1mat=cbind(-diag(1/t.s),
+              matrix(0,q,1))+cbind(matrix(0,q,1),
+                                   diag(1/t.s));
+  V1mat=D1mat%*%V%*%t(D1mat);
+  W.t=(yt[2:qp1]-yt[1:q])/t.s;
+  j1=matrix(1,q,1);
+  V1inv=ginv(V1mat);
+
+  #Trend parameter
+  theta.remle=(t(j1)%*%V1inv%*%W.t)/(t(j1)%*%V1inv%*%j1);
+
+  j=matrix(1,qp1,1);
+  Vinv=ginv(V);
+
+  #initial population
+  x0.remle=(t(j)%*%Vinv%*%(yt-as.numeric(theta.remle)*t.i))/(t(j)%*%Vinv%*%j);
+
+  #Extract REMLEs and AIC
+  remles        <- c(theta.remle,exp(optim.out$par[1:2]),x0.remle)
   lnL.hat     <- - optim.out$value[1]
   AIC         <- -2*lnL.hat + 2*2 #where 2 = length(REMLEs)...
 
